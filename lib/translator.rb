0a5ae861f1999f3dbc9d906c03974625c3a42625
Injae Lee <ilee.injae@gmail.com>
2020-04-13 18:02:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,25 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+ emoticons = YAML.load_file(file_path)
+ 
+  new_hash = {}
+  japanese = new_hash
+ emoticons.each do |meaning, translation|
+   english = translation[0]
+   japanese = translation[1]
+   new_hash[japanese] = meaning
+   new_hash["get_emoticon"][english] = japanese
+ end
+  return new_hash
+  binding.pry 
 end
 
+ 
+ 
+
+   
 def get_japanese_emoticon
   # code goes here
 end
