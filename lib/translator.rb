869696aa6ee3b0cab3d2df12ce51fd53cbd88083
Injae Lee <ilee.injae@gmail.com>
2020-04-13 22:07:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,10 +10,9 @@ def load_library(file_path)
     english = value[0]
     japanese = value[1]
     emoticon_lib[meaning] = inner_hash 
-    emoticon_lib[:english] = english
+    emoticon_lib[meaning][:english] = english
+    emoticon_lib[meaning][:japanese] = japanese
     
-    
-    binding.pry
   end
   emoticon_lib
 
