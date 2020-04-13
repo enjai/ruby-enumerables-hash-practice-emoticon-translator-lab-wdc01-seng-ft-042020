83e79c0c716c4141a4ed9a23ecfadf08e17ac376
Injae Lee <ilee.injae@gmail.com>
2020-04-13 23:39:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,4 +15,22 @@ def load_library(file_path)
 result 
 end
 
-   
+
+   def get_japanese_emoticon(file_path, japanese)
+emoticons = YAML.load_file(file_path)
+ 
+ emoticons.each do |meaning, value| 
+   dig value
+   value 
+   if the given value matches 
+     return the paired answer 
+      
+
+result = {} 
+
+
+end
+
+def get_english_meaning
+  # code goes here
+end
