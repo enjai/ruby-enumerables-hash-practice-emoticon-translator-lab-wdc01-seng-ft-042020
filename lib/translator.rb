18a979066a83bb5fdc36ff7d230863cc157fe71e
Injae Lee <ilee.injae@gmail.com>
2020-04-13 18:54:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,6 @@ def load_library(file_path)
  emoticons = YAML.load_file(file_path)
  
   new_hash = {}
-  japanese = new_hash
  emoticons.each do |meaning, translation|
    english = translation[0]
    japanese = translation[1]
